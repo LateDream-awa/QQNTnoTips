99220dac8e33307d5a52af61d1c804b6645ab8b4
LateDream <127290936+LateDreamXD@users.noreply.github.com>
2024-12-31 13:22:07 +0800

    🎈 perf: opt code

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 const {ipcMain} = require('electron');
-
-module.exports.onBrowserWindowCreated = w => {
-	// w is electron BrowserWindow instance
-
-	w.webContents.on('did-stop-loading', () => {
-		console.log('[QQNTnoTips: 后端] 新的窗口被创建, 开始检查');
-		w.webContents.send('check-window');
-		ipcMain.on('check-window-result', (event, result) => {
-			if(result) console.log('[QQNTnoTips: 后端] 检查未通过, 窗口已关闭');
-			else console.log('[QQNTnoTips: 后端] 检查通过, 窗口未关闭, 继续检查');
-		});
+module.exports.onBrowserWindowCreated = (window) => {
+	window.webContents.on('dom-ready', function() {
+		this.send('noTips.check');
 	});
 }
---
+++ renderer.js
@@ -0,0 +1,4 @@
+noTips.check(() => {
+	const target = document.querySelector('.pic-notification[type="poster"]');
+	if(!!target) window.close();
+});
